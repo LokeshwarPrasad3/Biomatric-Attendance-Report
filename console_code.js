4f347e47a0131bd00549093e844ef55364574692
LokeshwarMERN <lokeshwar.prasad.cse@gmail.com>
2024-05-08 00:57:14 +0530

    added 7 may attendance

--- console_code.js
+++ console_code.js
@@ -1,6 +1,6 @@
 // const lokeshwar = document.getElementById("studlist_tbody");
 // // Initialize totalDays as an empty array
-let totalDays = 67;
+let totalDays = 68;
 
 // // Get all <tr> elements inside the div
 // const rows = lokeshwar.getElementsByTagName("tr");
