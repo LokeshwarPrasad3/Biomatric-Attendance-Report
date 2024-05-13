8298b7be4afcd97fcaf705f6ad16a44e9d5cec64
LokeshwarMERN <lokeshwar.prasad.cse@gmail.com>
2024-05-14 00:41:17 +0530

    update till 14 May

--- console_code.js
+++ console_code.js
@@ -1,6 +1,6 @@
 // const lokeshwar = document.getElementById("studlist_tbody");
 // // Initialize totalDays as an empty array
-let totalDays = 68;
+let totalDays = 74;
 
 // // Get all <tr> elements inside the div
 // const rows = lokeshwar.getElementsByTagName("tr");
